e431f766e5fccf57d10af0b08d8068c65590fa47
kjwook7522 <kjwook7522@gmail.com>
2021-07-20 23:30:23 +0900

    Feat: introduce box 컴포넌트 추가

---
+++ src/components/molecules/IntroduceBox/IntroduceBox.styled.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const StyledIntroduceBox = styled.div`
+  padding: 3rem 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-top: 2px solid #fff;
+  border-bottom: 2px solid #fff;
+
+  .book-icon {
+    margin-bottom: 2rem;
+  }
+
+  .title {
+    line-height: 1.2;
+  }
+`
