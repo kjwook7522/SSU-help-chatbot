52ef0663f10db9719c13e9cfaf3f28efc912724b
kjwook7522 <kjwook7522@gmail.com>
2021-07-20 23:31:11 +0900

    Feat: index 페이지 완성

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,30 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import IndexTemplate from 'templates/IndexTemplate/ImdexTemplate';
+import IntroduceBox from 'components/molecules/IntroduceBox/IntroduceBox';
+import IndexBackground from 'components/organisms/Background/IndexBackground/IndexBackground';
+import SlideButton from 'components/atoms/Button/SlideButton/SlideButton';
 
-const Index: React.FC = () => <div>index</div>;
+const Index: React.FC = () => {
+  const history = useHistory();
+  return (
+    <>
+      <IndexBackground />
+      <IndexTemplate
+        introduceSection={<IntroduceBox />}
+        startButtonSection={
+          <SlideButton
+            backgroundColor="#3751A8"
+            hoverColor="#EE6684"
+            fontSize="1.25rem"
+            onClick={() => history.push('/handover')}
+          >
+            시작하기
+          </SlideButton>
+        }
+      />
+    </>
+  );
+};
 
 export default Index;
