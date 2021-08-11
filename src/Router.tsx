1685b4927634cd3220718f1ec90be2212ab1b2e5
김재욱 <kjwook7522@gmail.com>
2021-08-11 23:41:33 +0900

    Feat: 회원가입 페이지 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Index from 'pages/Index';
 import Login from 'pages/Login';
+import Join from 'pages/Join';
 import Handover from 'pages/Board/Handover';
 import CoBuying from 'pages/Board/CoBuying';
 import Recommend from 'pages/Board/Recommend';
@@ -15,6 +16,9 @@ const Router: React.FC = () => (
       </Route>
       <Route path="/login">
         <Login />
+      </Route>
+      <Route path="/join">
+        <Join />
       </Route>
       <Route path="/handover">
         <Handover />
