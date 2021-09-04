3a70ce2bb748638d0ff1e4737ca1b14e576adf10
김재욱 <kjwook7522@gmail.com>
2021-09-05 01:58:03 +0900

    Fix: book search form 적용

--- src/pages/HomeSearch.tsx
+++ src/pages/HomeSearch.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import BasicButton from 'components/atoms/Button/BasicButton/BasicButton';
 import { useAppDispatch } from 'common/hooks/userRedux';
 import { logOut } from 'actions/auth';
 import { removeToken } from 'utils/localstorage';
 import IndexBackground from 'components/organisms/Background/IndexBackground/IndexBackground';
 import CenterTemplate from 'templates/CenterTemplate/CenterTemplate';
+import BasicButton from 'components/atoms/Button/BasicButton/BasicButton';
+import BookSearchFrom from 'components/organisms/Form/BookSearchForm/BookSearchForm';
+
+import styled from 'styled-components';
 
 const HomeSearch: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -19,12 +22,21 @@ const HomeSearch: React.FC = () => {
       <CenterTemplate
         centerSection={
           <>
-            serach <BasicButton onClick={handleLogout}>로그아웃</BasicButton>
+            <BookSearchFrom />
+            <StyledLogoutButton>
+              <BasicButton onClick={handleLogout}>임시 로그아웃</BasicButton>
+            </StyledLogoutButton>
           </>
         }
       />
     </>
   );
 };
+
+const StyledLogoutButton = styled.div`
+  position: fixed;
+  top: 30px;
+  right: 30px;
+`;
 
 export default HomeSearch;
